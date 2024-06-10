b69f031635799fe74d2dccbfdd769183eca2dfd3
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-06-10 08:56:30 +0300

    added vid url to schema
    added vid url to schema  and pirs project

--- Frontend/src/Pages/Projects/projectes.ts
+++ Frontend/src/Pages/Projects/projectes.ts
@@ -1,8 +1,12 @@
-import abdimg1 from './unstructured_content/Abdu residence/Image 03.png'
+import pic from './unstructured_content/img/thumbnail.jpg'
+import vid1 from './unstructured_content/img/pirsvid.mp4'
 
 let projects = 
 [
-    {__id:"1",title:"",picUrl:"abdimg1",desc:""},
+    {__id:"1",title:"Reporting System",picUrl:pic,vidurl:vid1,desc:"In ethiopia there is a need for a reporting system. Noticing this problem me and my team tried to implement a reporting system in which the reports can be sorted by location, number of users and by the company that it is reported to. In this project we encountered multiple problems like making the user experience as frictionless as possible and trying to get as much information for the report. We tried to solve this problem by providing the user option to not repeat what other users have already reported. I learned a lot from this project like google maps api, browser geolocation api,  "},
+    {__id:"2",title:"Reporting System",picUrl:pic,vidurl:vid1,desc:"In ethiopia there is a need for a reporting system. Noticing this problem me and my team tried to implement a reporting system in which the reports can be sorted by location, number of users and by the company that it is reported to. In this project we encountered multiple problems like making the user experience as frictionless as possible and trying to get as much information for the report. We tried to solve this problem by providing the user option to not repeat what other users have already reported. I learned a lot from this project like google maps api, browser geolocation api,  "},
+    {__id:"3",title:"Reporting System",picUrl:pic,vidurl:vid1,desc:"In ethiopia there is a need for a reporting system. Noticing this problem me and my team tried to implement a reporting system in which the reports can be sorted by location, number of users and by the company that it is reported to. In this project we encountered multiple problems like making the user experience as frictionless as possible and trying to get as much information for the report. We tried to solve this problem by providing the user option to not repeat what other users have already reported. I learned a lot from this project like google maps api, browser geolocation api,  "},
+    
 ]
 
 export default projects
