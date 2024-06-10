b34a31c77fe2a4210ce1eae33376ceb8d6fe08fc
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-06-10 08:57:12 +0300

    updated project detail

--- Frontend/src/components/ProjectDetail/ProjectDetail.tsx
+++ Frontend/src/components/ProjectDetail/ProjectDetail.tsx
@@ -2,12 +2,14 @@ import styles from './styles.module.css'
 interface props{
     title: string,
     pic:string,
-    desc:string
+    desc:string,
+    vidurl: string
 }
-export default function ProjectDetail({title, pic, desc}:props){
+export default function ProjectDetail({title, pic, desc, vidurl}:props){
     return<>
         <figure className={styles.container}>
-            <img className={styles.pic} src={pic} alt={title+"pic"}/>
+            {(vidurl==null || vidurl=="")?<img className={styles.pic} src={pic} alt={title+"pic"}/>:<video autoPlay={true} src={vidurl}></video>}
+            
             <figcaption className={styles.caption}>
                 <h1 className={styles.title}>{title}</h1>
                 <p className={styles.desc}>{desc}</p>
