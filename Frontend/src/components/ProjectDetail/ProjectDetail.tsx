1b3583b24853e04c1966c19e4becdf09281b8daa
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-06-10 09:19:57 +0300

    added project hafcom

--- Frontend/src/components/ProjectDetail/ProjectDetail.tsx
+++ Frontend/src/components/ProjectDetail/ProjectDetail.tsx
@@ -8,7 +8,7 @@ interface props{
 export default function ProjectDetail({title, pic, desc, vidurl}:props){
     return<>
         <figure className={styles.container}>
-            {(vidurl==null || vidurl=="")?<img className={styles.pic} src={pic} alt={title+"pic"}/>:<video autoPlay={true} src={vidurl}></video>}
+            {(vidurl==null || vidurl=="")?<img className={styles.pic} src={pic} alt={title+"pic"}/>:<video className={styles.pic} autoPlay={true} src={vidurl}></video>}
             
             <figcaption className={styles.caption}>
                 <h1 className={styles.title}>{title}</h1>
